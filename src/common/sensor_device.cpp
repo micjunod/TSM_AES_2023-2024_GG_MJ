67c326253492a458fe7a35c41936a9334162cb9d
vocthor <pieceo108@gmail.com>
2023-10-17 15:52:32 +0200

    SensorDevice + start Speedometer

---
+++ src/common/sensor_device.cpp
@@ -0,0 +1,19 @@
+#include "sensor_device.hpp"
+#include <hdc1000.hpp>
+
+namespace bike_computer {
+
+    SensorDevice::SensorDevice() : _hdc1000(PD_13, PD_12, PC_6) {}
+
+    bool SensorDevice::init() {
+        return _hdc1000.probe();
+    }
+
+    float SensorDevice::readTemperature() {
+        return _hdc1000.getTemperature();
+    }
+
+    float SensorDevice::readHumidity() {
+        return _hdc1000.getHumidity();
+    }
+}
