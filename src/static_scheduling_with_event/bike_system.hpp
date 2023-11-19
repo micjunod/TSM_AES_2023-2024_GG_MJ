4cf5706555e83203e1ddab3f9f8914b7ac995c82
micjunod <146852720+micjunod@users.noreply.github.com>
2023-11-19 11:48:16 +0100

    Update bike_system.hpp

--- src/static_scheduling_with_event/bike_system.hpp
+++ src/static_scheduling_with_event/bike_system.hpp
@@ -103,10 +103,11 @@ class BikeSystem {
     advembsof::CPULogger _cpuLogger;
 
     //Event driven
-
+    ResetDevice _resetDevice;
     volatile bool _isReset = false;
     std::chrono::microseconds _resetTime = 0us;
 
+    
     volatile bool _isJoystickGear = false;
     std::chrono::microseconds _joystickGearTime = 0us;
 
